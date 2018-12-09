6d9f0ece0c467d9b9e4bff5e254e60d661c24550
narek12345 <nareksahakyan4@gmail.com>
2018-12-10 03:29:07 +0400

    sign out in backend

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :desks
   resources :bookings
   resources :rooms
-  resources :members
+  resources :members, only: [:create,:index, :show, :update, :destroy]
   resources :sessions, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
