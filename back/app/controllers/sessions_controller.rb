e7db193173fb8e19173c8b03a06d3c20b7588771
narek12345 <nareksahakyan4@gmail.com>
2018-12-09 05:03:00 +0400

    token authentication

--- back/app/controllers/sessions_controller.rb
+++ back/app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
     def create
         @user = User.where(email: params[:email]).first
         if @user&.valid_password?(params[:password])
-        render json: @user.as_json(only: [:id,:email])
+        render json: @user.as_json(only: [:id,:email,:authentication_token])
         else
             head(:unauthorized)
         end
