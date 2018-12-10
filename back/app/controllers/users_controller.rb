880a820622fda40ecca89b5832df0d0373e3e54a
ArevikHayrapetyan7 <hayrapetyanarev99@gmail.com>
2018-12-10 21:36:45 +0400

    WIP : admin page

--- back/app/controllers/users_controller.rb
+++ back/app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.admin?
-       @user.update(member_params)
+       @user.update(user_params)
        render json: @user
     else
       render json: status: :unauthorized
