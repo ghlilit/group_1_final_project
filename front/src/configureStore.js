1015017034ad9e03fc2c2412a117236e7a0faa02
ArevikHayrapetyan7 <hayrapetyanarev99@gmail.com>
2018-12-13 19:30:46 +0400

    fix: some error

--- front/src/configureStore.js
+++ front/src/configureStore.js
@@ -2,6 +2,6 @@ import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 export default store;
