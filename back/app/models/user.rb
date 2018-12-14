31876b278d203c59c0e0012fa45e4ed7c2fa97b2
narek12345 <nareksahakyan4@gmail.com>
2018-12-15 03:07:36 +0400

    validations for sign up

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 # Include default devise modules.
  validates :role, presence: true
+ validates :fname, format: { with: /\A[a-zA-Z0-9]+\Z/ }, presence: true
+ validates :lname, format: { with: /\A[a-zA-Z0-9]+\Z/ }, presence: true
  has_many :bookings, dependent: :destroy
  devise :database_authenticatable, :registerable,
        :recoverable, :rememberable, :validatable,:trackable
