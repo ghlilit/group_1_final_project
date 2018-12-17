ac295c0073a0f6f60dc2b1109eaf902686216ae8
narek12345 <nareksahakyan4@gmail.com>
2018-12-17 15:41:11 +0400

    feat: validating existing booking in server

--- back/app/models/booking.rb
+++ back/app/models/booking.rb
@@ -2,4 +2,12 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :room
   belongs_to :timespot
+  validate :checkavailable
+   def checkavailable
+     res = Booking.where(timespot_id: timespot_id, book_date: book_date, room_id: room_id)
+     p res.first
+      if res.first != nil
+        errors.add(:book_date, "A booking exists")
+    end
+  end
 end
